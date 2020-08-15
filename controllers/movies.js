b2b5cddac238454bac83a7bce45d2d3d9c95c11b
davidedsams <davidedsams@gmail.com>
2020-08-15 19:31:27 -0400

    Add routes for all the endpoints from the planning document in README.

--- controllers/movies.js
+++ controllers/movies.js
@@ -3,22 +3,40 @@ const Movie = require('../models/Movie');
 const router = express.Router();
 
 // Get all the movies
-router.get('/', (req, res)=>{
-    Movie.find({}).then((movies)=>{
-        res.json(movies);
-    })
-})
+router.get('/', (req, res) => {
+	Movie.find({}).then((allMovies) => {
+		res.json(allMovies);
+	});
+});
 
 // Create a new movie
-router.post('/', (req, res) => {});
+router.post('/', (req, res) => {
+	let newMovie = req.body;
+	Movie.create(newMovie).then(() => {
+		res.json(newMovie);
+	});
+});
 
 // Update a movie by title
-router.put('/:title', (req, res) => {});
+router.put('/:title', (req, res) => {
+	let updatedMovie = req.body;
+	Movie.findOneAndUpdate({ title: req.params.title }, updatedMovie, {
+		new: true,
+	}).then((updatedMovie) => res.json(updatedMovie));
+});
 
 // Get a movie by title
-router.get('/', (req, res) => {});
+router.get('/:title', (req, res) => {
+	Movie.find({ title: req.params.title }).then((movieByTitle) => {
+		res.json(movieByTitle);
+	});
+});
 
 // Delete a movie by title
-router.delete('/', (req, res) => {});
+router.delete('/:title', (req, res) => {
+	Movie.findOneAndDelete({ title: req.params.title }).then((deletedMovie) => {
+		res.json(deletedMovie);
+	});
+});
 
 module.exports = router;
