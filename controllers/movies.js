e00327465575cdda74ce9802c18708d75e5d9f5c
davidedsams <davidedsams@gmail.com>
2020-08-15 19:09:01 -0400

    Add pseudocode and templates for routes.

--- controllers/movies.js
+++ controllers/movies.js
@@ -2,10 +2,23 @@ const express = require('express');
 const Movie = require('../models/Movie');
 const router = express.Router();
 
+// Get all the movies
 router.get('/', (req, res)=>{
     Movie.find({}).then((movies)=>{
         res.json(movies);
     })
 })
+
+// Create a new movie
+router.post('/', (req, res) => {});
+
+// Update a movie by title
+router.put('/:title', (req, res) => {});
+
+// Get a movie by title
+router.get('/', (req, res) => {});
+
+// Delete a movie by title
+router.delete('/', (req, res) => {});
 
 module.exports = router;
