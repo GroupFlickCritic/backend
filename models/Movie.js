393648a5d867370cb4e9cc703a6731552ec44148
alishalawani <alishalawani@gmail.com>
2020-08-15 20:35:06 -0500

    modified the model

--- models/Movie.js
+++ models/Movie.js
@@ -1,11 +1,12 @@
 const mongoose = require('../db/connection.js');
 
-const MovieSchema = new mongoose.Schema({
+const MovieSchema = mongoose.Schema({
     title: {type: String, required: true},
     mainImage: String,
     infoImage: String,
     movieInfo: {summary: String, director: String, writers: String, genres: String, rated: String, releaseData: String},
     reviews: [{review: String, datePosted: Date}]
+
 });
 
 const Movie = mongoose.model('Movie', MovieSchema);
