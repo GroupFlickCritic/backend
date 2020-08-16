1a359ab4d5d55fdcba059349cf53b595e7fbdf94
alishalawani <alishalawani@gmail.com>
2020-08-16 10:34:40 -0500

    changed the mongoURi

--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = Promise;
 
-const mongoURI = 'mongodb://localhost/flick-critic';
+const mongoURI =
+	process.env.NODE_ENV === 'production'
+		? process.env.DB_URL
+		: 'mongodb://localhost/flick-critic';
 
 mongoose.connect(mongoURI, {useNewUrlParser: true}).then((conn)=>{
     console.log(`connected to mongodb on ${conn.connections[0].name} db`)
