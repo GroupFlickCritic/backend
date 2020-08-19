0fb3ad409e57c2a38e7e612b3837290003bde0e3
alishalawani <alishalawani@gmail.com>
2020-08-19 10:46:21 -0500

    set new routes

--- models/Review.js
+++ models/Review.js
@@ -3,10 +3,10 @@ const mongoose = require('../db/connection');
 const ReviewSchema = mongoose.Schema({
 	review: String,
 	datePosted: Date,
-	movie: {
+	movie: [{
 		ref: 'Movie',
-		type: mongoose.Schema.Types.ObjectId,
-	},
+		type: mongoose.Schema.Types.ObjectId
+	}]
 });
 const Review = mongoose.model('Review', ReviewSchema);
 module.exports = Review;
