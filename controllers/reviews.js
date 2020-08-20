6f77763615c6f3b86bd9943f256748d13b599125
alishalawani <alishalawani@gmail.com>
2020-08-20 09:52:09 -0500

    added a new route to the reviews controller

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const Review = require('../models/Review');
+const Movie = require('../models/Movie');
 const router = express.Router();
 
 router.get('/', (req, res) => {
@@ -10,18 +11,20 @@ router.get('/', (req, res) => {
 		.catch(console.error);
 });
 
-router.get('/:id', (req, res)=>{
-	Review.findById({_id: req.params.id}).then((review)=>{
-		res.json(review);
-	}).catch(console.error)
-})
+router.get('/:id', (req, res) => {
+	Review.findById({ _id: req.params.id })
+		.then((review) => {
+			res.json(review);
+		})
+		.catch(console.error);
+});
 //create a new review
 router.post('/', (req, res) => {
 	let newReview = req.body;
 	Review.create(newReview)
 		.then((theNewReview) => {
 			//return the created review
-					res.json(theNewReview);
+			res.json(theNewReview);
 		})
 		.catch(console.error);
 });
@@ -42,14 +45,26 @@ router.put('/:id', (req, res) => {
 		.catch(console.error);
 });
 
-router.delete('/:id', (req, res) => {
-	Review.findOneAndDelete({ _id: req.params.id }).then((deletedReview) => {
-		Review.find({})
-			.then((allReviews) => {
-				res.json(allReviews);
-			})
-			.catch(console.error);
+// router.delete('/:id', (req, res) => {
+// 	Review.findOneAndDelete({ _id: req.params.id }).then((deletedReview) => {
+// 		Review.find({})
+// 			.then((allReviews) => {
+// 				res.json(allReviews);
+// 			})
+// 			.catch(console.error);
+// 	});
+// });
+
+//delete a review by its id and delete the review from it's movie reviews array
+router.delete('/:id/:movieId', (req, res) => {
+	let newMovie = req.body;
+
+	Movie.findOneAndUpdate({ _id: req.params.movieId }, newMovie, {new: true}).then(() => {
+		Review.findOneAndDelete({
+			_id: req.params.id,
+		}).then((deletedReview) => {
+			res.json(deletedReview);
+		});
 	});
 });
-
 module.exports = router;
